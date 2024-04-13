8a23238f53921c5d7c69a26fb8914f85a8d2d8da
yiqunchen1999 <yiqunchen1999@icloud.com>
2024-04-13 12:35:03 +0800

    support read default configs from configs.json

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
+from importlib.metadata import metadata
 import json
 import os
 import logging
 import datetime
 import os.path as osp
+from functools import lru_cache
 from dataclasses import dataclass, field
 
 import arxiv
@@ -17,11 +19,11 @@ logger = logging.getLogger(__name__)
 
 
 DOCUMENTS = dict(
-    categories=("Categories of desired papers with the format `cat:[CAT_ID]`. "
-                "For example, `cat:cs.CV`. "
+    categories=("Categories of desired papers with the format `(cat:[CAT_ID])`"
+                ". For example, `(cat:cs.CV)`. "
                 "See: https://arxiv.org/category_taxonomy for more details. "
                 "Use `AND`, `OR`, etc to specify multiple categories, e.g., "
-                "`cat:cs.CV OR cat:cs.AI OR cat:cs.LG`."),
+                "`(cat:cs.CV OR cat:cs.AI OR cat:cs.LG)`."),
     datetime=("The date and time range, if not specified, this script search"
               "papers updated from `YESTERDAY:00:00 TO TODAY:00:00`. "
               "The format should be `YYYYMMDDHHMM TO YYYYMMDDHHMM`, "
@@ -30,6 +32,8 @@ DOCUMENTS = dict(
     keywords=("Also save papers that contain keyword to seperate files. "
               "Different keywords should be seperated by `|`, e.g., "
               "`detect|segment`."),
+    output_directory=("The directory to save the outputs. "),
+    markdown_directory=("The directory to save the markdown (table) files."),
     query=("If you're familar with arxiv api search query, you can directly "
            "specify the search query. All the above items will be ignored."),
 )
@@ -38,20 +42,54 @@ DOCUMENTS = dict(
 TABLE_HEADERS = (
     'title', 'primary category', 'paper abstract link', 'paper pdf link'
 )
+DEFAULT_KEYWORDS = [
+    "detect", "diffusion", "segment",
+
+    "vision", "visual",
+    "vision-language", "vision language",
+    "multimodal", "multi-modal", "multi modal", 
+    "vlm", "mllm",
+
+    "segment&vision", "segment&visual",
+    "segment&vision-language", "segment&vision language",
+    "segment&multimodal", "segment&multi-modal", "segment&multi modal",
+    "segment&vlm", "segment&mllm"
+]
+
+
+@lru_cache
+def default_configs():
+    if not osp.exists('configs.json'):
+        return {}
+    with open('configs.json', 'r') as fp:
+        configs = json.load(fp)
+    configs['keywords'] = '|'.join(configs['keywords'])
+    return configs
 
 
 @dataclass
 class Configs:
-    categories: str = field(default="cat:cs.CV OR cat:cs.AI OR cat:cs.LG",
-                            metadata={"help": DOCUMENTS["categories"]})
-    num_retries: int = 10
+    categories: str = field(
+        default=default_configs().get(
+            'categories', '(cat:cs.CV OR cat:cs.AI OR cat:cs.LG)'),
+        metadata={"help": DOCUMENTS["categories"]})
+    num_retries: int = field(
+        default=default_configs().get('num_retries', 10))
     keywords: str = field(
-        default="detect|diffusion|segment",
+        default=default_configs().get('keywords', '|'.join(DEFAULT_KEYWORDS)),
         metadata={"help": DOCUMENTS["keywords"]})
-    datetime: str = field(default=None,
-                          metadata={"help": DOCUMENTS["datetime"]})
-    output_directory: str = "outputs"
-    query: str = field(default=None, metadata={"help": DOCUMENTS["query"]})
+    datetime: str = field(
+        default=default_configs().get('datetime', None),
+        metadata={"help": DOCUMENTS["datetime"]})
+    output_directory: str = field(
+        default=default_configs().get('output_directory', 'outputs'),
+        metadata={"help": DOCUMENTS["output_directory"]})
+    markdown_directory: str = field(
+        default=default_configs().get('markdown_directory', 'markdown'),
+        metadata={"help": DOCUMENTS["markdown_directory"]})
+    query: str = field(
+        default=default_configs().get('query'),
+        metadata={"help": DOCUMENTS["query"]})
 
     def __post_init__(self):
         self.datetime = parse_date(self.datetime)
@@ -60,7 +98,10 @@ class Configs:
             self.query = f"{self.categories} AND {self.datetime}"
         self.output_directory = osp.join(self.output_directory,
                                          self.datetime.split('[')[1][:8])
+        self.markdown_directory = osp.join(self.markdown_directory,
+                                           self.datetime.split('[')[1][:8])
         os.makedirs(self.output_directory, exist_ok=True)
+        os.makedirs(self.markdown_directory, exist_ok=True)
         setup_logger(self.output_directory)
 
 
@@ -74,14 +115,16 @@ def search_and_parse(cfgs: Configs):
     results = search(cfgs)
     jsonl = convert_results_to_dict(results)
     save_results_to_jsonl(jsonl, cfgs.output_directory)
-    save_markdown_table(jsonl, cfgs.output_directory, TABLE_HEADERS)
+    save_markdown_table(jsonl, cfgs.markdown_directory, TABLE_HEADERS)
 
     path = osp.join(cfgs.output_directory, "papers.txt")
     logger.info(f'Saving {len(results)} results to {path}')
     with open(path, 'w') as fp:
         fp.writelines([format_result(r, i)
                        for i, r in enumerate(results)])
-    save_by_keywords(results, cfgs.keyword_list, cfgs.output_directory)
+    save_by_keywords(results, cfgs.keyword_list,
+                     cfgs.output_directory,
+                     cfgs.markdown_directory)
 
 
 def save_markdown_table(results: list[dict],
@@ -90,11 +133,11 @@ def save_markdown_table(results: list[dict],
                         suffix: str = ""):
     headers = headers or list(results[0].keys())
     makrdown = make_markdown_table(results, headers)
-    suffix = "" if suffix == "" else f"-{suffix}"
+    suffix = "" if suffix == "" else f" @ {suffix}"
     path = osp.join(output_directory, f"papers{suffix}.md")
     logger.info(f'Saving markdown table to {path}')
     with open(path, 'w') as fp:
-        fp.write('\n' + '# Paper List\n\n')
+        fp.write('\n')
         fp.write(makrdown)
         fp.write('\n\n')
     return path
@@ -140,24 +183,39 @@ def convert_results_to_dict(results: list[arxiv.Result]) -> list[dict]:
 
 def save_by_keywords(results: list[arxiv.Result],
                      keywords: list[str],
-                     output_directory: str):
+                     output_directory: str,
+                     markdown_directory: str):
     for keyword in keywords:
-        save_by_keyword(results, keyword, output_directory)
+        save_by_keyword(results, keyword, output_directory, markdown_directory)
 
 
 def save_by_keyword(results: list[arxiv.Result],
                     keyword: str,
-                    output_directory: str):
-    results = list(
-        filter(lambda r: (keyword in r.summary.lower()
-                          or keyword in r.title.lower()),
-               results)
-    )
+                    output_directory: str,
+                    markdown_directory: str):
+    if '&' in keyword:
+        keywords = keyword.split('&')
+        keywords = [kw.strip() for kw in keywords]
+        results = list(
+            filter(
+                lambda r: all(kw in r.summary.lower() or kw in r.title.lower()
+                              for kw in keywords),
+                results
+            )
+        )
+    else:
+        results = list(
+            filter(
+                lambda r: (keyword in r.summary.lower()
+                           or keyword in r.title.lower()),
+                results
+            )
+        )
     jsonl = convert_results_to_dict(results)
-    save_markdown_table(jsonl, output_directory,
+    save_markdown_table(jsonl, markdown_directory,
                         headers=TABLE_HEADERS,
                         suffix=keyword)
-    path = osp.join(output_directory, f'keyword-{keyword}.txt')
+    path = osp.join(output_directory, f'keyword @ {keyword}.txt')
     logger.info(f'[{keyword}]: Saving {len(results)} results to {path}')
     with open(path, 'w') as fp:
         fp.writelines([format_result(r, i) for i, r in enumerate(results)])
@@ -191,6 +249,7 @@ def parse_date(date: str = None):
         today = datetime.date.today()
         yesterday = today - datetime.timedelta(days=1)
         date = f"{yesterday.strftime('%Y%m%d%H%M')} TO {today.strftime('%Y%m%d%H%M')}"
+    date = date.replace('-', '').replace(':', '')
     date = f"lastUpdatedDate:[{date}]"
     return date
 
--- markdown.py
+++ markdown.py
@@ -3,7 +3,8 @@ def make_markdown_table(contents: list[dict],
                         headers: list[str] = None) -> str:
     headers = headers or list(contents[0].keys())
     table = f"| {' | '.join(headers)} |\n"
-    table += f"| {' | '.join(['---' for _ in headers])} |\n"
-    for row in contents:
-        table += f"| {' | '.join([str(row[h]) for h in headers])} |\n"
+    table = '| Index ' + table
+    table += f"| --- | {' | '.join(['---' for _ in headers])} |\n"
+    for idx, row in enumerate(contents):
+        table += f"| {idx+1} | {' | '.join([str(row[h]) for h in headers])} |\n"
     return table
