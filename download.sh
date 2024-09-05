536a40e31b6003f3d5aa9b54eee7f87c6c3edc61
yiqunchen1999 <yiqunchen1999@163.com>
2024-09-05 21:57:48 +0800

    [NOTE] I'll clean and refactor the code recently

--- download.sh
+++ download.sh
@@ -2,124 +2,119 @@
 # python download.py \
 #     --markdown_subfolder "Vision-Language" \
 #     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2408.03940 \
-#     --tags "vision-language,segmentation,insight"
+#     --link https://arxiv.org/abs/2407.20756 \
+#     --tags "vision-language,synthetic,data" \
+#     --code_link https://github.com/starriver030515/SynthVLM
 
 # python download.py \
-#     --markdown_subfolder "Vision" \
+#     --markdown_subfolder "NLP" \
 #     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2405.15636 \
-#     --tags "GAN,insight" \
-#     --code_link "https://github.com/rherdt185/visualize-gan-activations"
+#     --link https://arxiv.org/abs/2307.11088 \
+#     --tags "benchmark,evaluation" \
+#     --code_link https://github.com/OpenLMLab/LEval
+
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2406.13642 \
+#     --tags "vision-language,robot"  \
+#     --code_link https://github.com/BAAI-DCAI/SpatialBot
+
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2407.07053 \
+#     --tags "synthetic,data,vision-language" \
+#     --code_link https://github.com/zwq2018/Multi-modal-Self-instruct
+
+# python download.py \
+#     --markdown_subfolder "NLP" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2407.21787 \
+#     --tags "insight"
+
+# python download.py \
+#     --markdown_subfolder "NLP" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2310.19240 \
+#     --tags "benchmark" \
+#     --code_link https://github.com/KwanWaiChung/M4LE
+
+# python download.py \
+#     --markdown_subfolder "NLP" \
+#     --journal "ACL'24" \
+#     --link https://arxiv.org/abs/2402.00159 \
+#     --tags "dataset" \
+#     --code_link https://github.com/allenai/dolma
+
+# python download.py \
+#     --markdown_subfolder "NLP" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2406.15126 \
+#     --tags "survey,synthetic,data,evaluation"
+
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2408.15205 \
+#     --tags "vision-language,segmentation" \
+#     --code_link https://lwpyh.github.io/ProMaC/
 
 # python download.py \
 #     --markdown_subfolder "Vision" \
 #     --journal "ECCV'24" \
-#     --link https://arxiv.org/abs/2403.19588 \
-#     --tags "segmentation,insight"  \
-#     --code_link https://github.com/naver-ai/rdnet
-
-# python download.py \
-#     --markdown_subfolder "Vision" \
-#     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2408.03806 \
+#     --link https://arxiv.org/abs/2408.15201 \
 #     --tags "insight"
 
 # python download.py \
 #     --markdown_subfolder "Vision" \
 #     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2408.04471 \
-#     --tags "segmentation,insight" \
-#     --code_link https://europe.naverlabs.com/
-
-# python download.py \
-#     --markdown_subfolder "Vision" \
-#     --journal "ACMMM'24" \
-#     --link https://arxiv.org/abs/2408.04326 \
-#     --tags "segmentation" \
-#     --code_link https://github.com/BellyBeauty/MDSAM
+#     --link https://arxiv.org/abs/2408.15178 \
+#     --tags "survey"
 
 # python download.py \
 #     --markdown_subfolder "Vision" \
 #     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2406.18037 \
-#     --tags "segmentation,continual-learning,incremental-learning" \
-#     --code_link https://github.com/dyxu-cuhkcse/SMG-Learning
-
-# python download.py \
-#     --markdown_subfolder "Vision-Language" \
-#     --journal "ECCV'24" \
-#     --link https://arxiv.org/abs/2408.05097 \
-#     --tags "vision-language,segmentation" \
-#     --code_link https://italailabs.com/
-
-# python download.py \
-#     --markdown_subfolder "Vision-Language" \
-#     --journal "ECCV'24" \
-#     --link https://arxiv.org/abs/2408.04961 \
-#     --tags "vision-language,open-vocabulary,segmentation" \
-#     --code_link https://cvlab.postech.ac.kr/research/lazygrounding
-
-# python download.py \
-#     --markdown_subfolder "Vision-Language" \
-#     --journal "ECCV'24" \
-#     --link https://arxiv.org/abs/2408.04883 \
-#     --tags "vision-language,open-vocabulary,segmentation" \
-#     --code_link https://github.com/mc-lan/ProxyCLIP
-
-# python download.py \
-#     --markdown_subfolder "Vision-Language" \
-#     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2408.02034v2 \
-#     --tags "vision-language,segmentation" \
-#     --code_link https://github.com/Yuliang-Liu/Monkey
+#     --link https://arxiv.org/abs/2408.14957 \
+#     --tags "segmentation,few-shot"
 
 # python download.py \
 #     --markdown_subfolder "Vision" \
 #     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2408.06305 \
-#     --tags "segmentation,insight"
-
-# python download.py \
-#     --markdown_subfolder "Vision" \
-#     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2408.06021 \
-#     --tags "segmentation" \
-#     --code_link https://github.com/hahamyt/ClickAttention
+#     --link https://arxiv.org/abs/2408.16504 \
+#     --tags "segmentation"
 
 # python download.py \
 #     --markdown_subfolder "Vision" \
 #     --journal "ECCV'24" \
-#     --link https://arxiv.org/abs/2312.10105 \
+#     --link https://arxiv.org/abs/2408.16310 \
 #     --tags "segmentation,foundation" \
-#     --code_link https://github.com/naver-ai/seit
+#     --code_link http://github.com/lytang63/SlotSAM
 
 # python download.py \
-#     --markdown_subfolder "Vision-Language" \
-#     --journal "IJCV'24" \
-#     --link https://arxiv.org/abs/2305.18279v2 \
-#     --tags "vision-language,detection,segmentation" \
-#     --code_link https://github.com/yuhangzang/ContextDET
-
-# python download.py \
-#     --markdown_subfolder "Vision-Language" \
+#     --markdown_subfolder "NLP" \
 #     --journal "arxiv" \
-#     --link https://arxiv.org/abs/2404.09447 \
-#     --tags "vision-language,open-vocabulary,segmentation,training-free"
+#     --link https://arxiv.org/abs/2406.06391 \
+#     --tags "survey,continual-learning,incremental-learning" \
+#     --code_link https://github.com/qianlima-lab/awesome-lifelong-learning-methods-for-llm
 
 # python download.py \
-#     --markdown_subfolder "Vision-Language" \
-#     --journal "CVPR'24" \
-#     --link https://arxiv.org/abs/2408.06747 \
-#     --tags "vision-language,segmentation,unsupervised,insight" \
-#     --code_link https://github.com/dogehhh/ReCLIP
+#     --markdown_subfolder "NLP" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2407.15017 \
+#     --tags "survey,insight"
 
 # python download.py \
-#     --markdown_subfolder "Vision" \
-#     --journal "WACV'24" \
-#     --link https://arxiv.org/abs/2408.07576 \
-#     --tags "segmentation,efficient" \
-#     --code_link https://github.com/hyunwoo137/MetaSeg
+#     --markdown_subfolder "RL" \
+#     --journal "CoLLAs'24" \
+#     --link https://arxiv.org/abs/2405.19047 \
+#     --tags "RL"
+
+python download.py \
+    --markdown_subfolder "Vision-Language" \
+    --journal "TPAMI'24" \
+    --link 2304.07547 \
+    --tags "segmentation,open-vocabulary,vision-language"
 
 # python download.py \
 #     --markdown_subfolder "Vision-Language" \
@@ -133,101 +128,101 @@
 #     --link https://arxiv.org/abs/2408.07393 \
 #     --tags "segmentation,few-shot,segment-anything"
 
-python download.py \
-    --markdown_subfolder "Vision-Language" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2312.13764 \
-    --tags "vision-language,segmentation" \
-    --code_link https://github.com/lambert-x/ProLab
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2312.13764 \
+#     --tags "vision-language,segmentation" \
+#     --code_link https://github.com/lambert-x/ProLab
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "CVPR'24" \
-    --link https://arxiv.org/abs/2408.07243 \
-    --tags "dataset,distillation"
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "CVPR'24" \
+#     --link https://arxiv.org/abs/2408.07243 \
+#     --tags "dataset,distillation"
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "ECCV'24" \
-    --link https://arxiv.org/abs/2402.17514 \
-    --tags "segmentation,zero-shot,robustness"
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "ECCV'24" \
+#     --link https://arxiv.org/abs/2402.17514 \
+#     --tags "segmentation,zero-shot,robustness"
 
-python download.py \
-    --markdown_subfolder "Vision-Language" \
-    --journal "TMM" \
-    --link https://arxiv.org/abs/2404.07600 \
-    --tags "vision-language,segmentation,diffusion"
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "TMM" \
+#     --link https://arxiv.org/abs/2404.07600 \
+#     --tags "vision-language,segmentation,diffusion"
 
-python download.py \
-    --markdown_subfolder "Vision-Language" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2408.08862 \
-    --tags "vision-language,segmentation"
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2408.08862 \
+#     --tags "vision-language,segmentation"
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://vpariza.github.io/NeCo/ \
-    --tags "segmentation"
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://vpariza.github.io/NeCo/ \
+#     --tags "segmentation"
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2311.13254 \
-    --tags "segmentation,domain"
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2311.13254 \
+#     --tags "segmentation,domain"
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "IJCV" \
-    --link https://arxiv.org/abs/2306.02314 \
-    --tags "segmentation,efficient"
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "IJCV" \
+#     --link https://arxiv.org/abs/2306.02314 \
+#     --tags "segmentation,efficient"
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2310.12431 \
-    --tags "segmentation,segment-anything"
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2310.12431 \
+#     --tags "segmentation,segment-anything"
 
-python download.py \
-    --markdown_subfolder "Vision-Language" \
-    --journal "ACCV'24" \
-    --link https://arxiv.org/abs/2310.13479 \
-    --tags "segmentation,weakly-supervised,vision-language" \
-    --code_link https://github.com/fgirbal/segment-select-correct
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "ACCV'24" \
+#     --link https://arxiv.org/abs/2310.13479 \
+#     --tags "segmentation,weakly-supervised,vision-language" \
+#     --code_link https://github.com/fgirbal/segment-select-correct
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2408.11535 \
-    --tags "segmentation,ICL" 
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2408.11535 \
+#     --tags "segmentation,ICL" 
 
-python download.py \
-    --markdown_subfolder "Vision-Language" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2408.12489 \
-    --tags "segmentation,scribble" \
-    --code_link https://github.com/wbkit/Scribbles4All
+# python download.py \
+#     --markdown_subfolder "Vision-Language" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2408.12489 \
+#     --tags "segmentation,scribble" \
+#     --code_link https://github.com/wbkit/Scribbles4All
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://arxiv.org/pdf/2408.12364 \
-    --tags "segmentation,segment-anything" 
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/pdf/2408.12364 \
+#     --tags "segmentation,segment-anything" 
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2408.12957 \
-    --tags "survey,foundation,segmentation,segment-anything" 
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2408.12957 \
+#     --tags "survey,foundation,segmentation,segment-anything" 
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2408.14032 \
-    --tags "open-set,detection,segmentation" 
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2408.14032 \
+#     --tags "open-set,detection,segmentation" 
 
-python download.py \
-    --markdown_subfolder "Vision" \
-    --journal "arxiv" \
-    --link https://arxiv.org/abs/2408.13980 \
-    --tags "segmentation,segment-anything" 
+# python download.py \
+#     --markdown_subfolder "Vision" \
+#     --journal "arxiv" \
+#     --link https://arxiv.org/abs/2408.13980 \
+#     --tags "segmentation,segment-anything" 
