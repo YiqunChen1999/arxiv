d3546441ec29a850be9b8c73bda281144225578a
yiqunchen <yiqunchen@icloud.com>
2025-01-22 11:17:51 +0800

    [FIX] wrong paper path

--- arxiver/plugins/downloader.py
+++ arxiver/plugins/downloader.py
@@ -330,7 +330,7 @@ def prepare_markdown_content(
     else:
         # abspath = osp.abspath(osp.join(download_directory, filename))
         # abspath = "file://" + abspath.replace(" ", "%20")
-        path = osp.join("../../Papers", filename)
+        path = osp.join("../../Papers", filename).replace(" ", "%20")
         temp = PAPER_NOTE_TEMPLATE.format(filename, path)
     content = meta + OBSIDIAN_NAVIGATION + temp
     return content
