839fa41f055461738fa40c2a122b94af64b31813
yiqunchen <yiqunchen@icloud.com>
2025-02-10 22:47:02 +0800

    [FEATURE] support first filter based on default (rule-based) keywords filter

--- arxiver/plugins/language_model_based_keywords_filter.py
+++ arxiver/plugins/language_model_based_keywords_filter.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 from arxiver.utils.logging import create_logger
 from arxiver.base.plugin import (
-    BasePlugin, BaseKeywordsFilterData, GlobalPluginData
+    BasePlugin, BaseKeywordsFilterData, BasePluginData, GlobalPluginData
 )
 from arxiver.base.result import Result
 from arxiver.core.agent import Agent
@@ -19,31 +19,35 @@ def plugin_name():
 def default_prompt_template():
     return (
         ""
-        "# Task\n"
-        # Description
-        "## Description\n"
-        "You are given a research paper's title and abstract. Your task is to "
-        "determine whether the paper is **explicitly focused on** "
-        "the {topic}.\n\n"
+        "# Task Description\n"
+        "You are given a research paper's title and abstract as inputs. "
+        "Your task is to determine whether the paper is falling into "
+        "the interested topic.\n\n"
 
         # Requirements
-        "## Requirements\n"
+        "# Task Requirements\n"
         "- Please first provide a clear analysis "
         "based on the title and abstract;\n"
 
         "- You shouldn't try to guess the content not appear in "
         "the provided text;\n"
 
-        "- State your conclusion in the following format: "
-        "**RESULT: [YOUR RESULT]**, "
-        "where [YOUR RESULT] is either TRUE or FALSE;\n"
+        "- If the paper is related to the interested topic, "
+        "return ""**RESULT: TRUE**;\n"
+
+        "- If the paper is related to the discarded topic, "
+        "return ""**RESULT: FALSE**, even if the paper is also related to "
+        "the interested topic;\n"
 
         "- If the paper does not fall into the interested scope, return "
         "**RESULT: FALSE**;\n\n"
 
         # Title and Abstract
-        "# Title\n{title}\n\n"
-        "# Abstract\n{abstract}"
+        "# Task Input\n"
+        "## Interested Topic\n{interested}\n\n"
+        "## Discarded Topic\n{discarded}\n\n"
+        "## Title\n{title}\n\n"
+        "## Abstract\n{abstract}"
     )
 
 
@@ -74,13 +78,15 @@ class LanguageModelBasedKeywordsFilter(BasePlugin):
             model: str,
             batch_mode: bool,
             concurrent_mode: bool,
-            topics: dict[str, str],
+            interested_topics: dict[str, str],
+            discarded_topics: dict[str, str],
             max_workers: int = 16,
             max_tasks_per_minute: int = 16):
         self.agent = Agent(model)
         self.batch_mode = batch_mode
         self.concurrent_mode = concurrent_mode
-        self.topics = topics
+        self.interested_topics = interested_topics
+        self.discarded_topics = discarded_topics
         self.max_workers = max_workers
         self.max_tasks_per_minute = max_tasks_per_minute
 
@@ -103,11 +109,12 @@ class LanguageModelBasedKeywordsFilter(BasePlugin):
             return results
         N = len(results_to_process)
         logger.info(f"Processing {N} results in batch...")
-        for keyword, topic in self.topics.items():
-            logger.info(f"Creating prompts related to {topic}...")
-            prompts.extend([
-                self.create_prompt(topic, r) for r in results_to_process
-            ])
+        for keyword, interested in self.interested_topics.items():
+            logger.info(f"Creating prompts related to {interested}...")
+            discarded = self.discarded_topics.get(keyword, "")
+            prompts.extend(
+                prepare_prompts(results_to_process, interested, discarded)
+            )
         logger.info("Sending prompts to the agent...")
         complete_method = (
             self.agent.complete_batches if self.batch_mode
@@ -115,7 +122,7 @@ class LanguageModelBasedKeywordsFilter(BasePlugin):
         )
         responses = complete_method(prompts)
         logger.info("Processing responses...")
-        keywords = list(self.topics.keys())
+        keywords = list(self.interested_topics.keys())
         for i, r in enumerate(responses):
             if "**RESULT: TRUE**" in r:
                 result = results_to_process[i % N]
@@ -123,40 +130,6 @@ class LanguageModelBasedKeywordsFilter(BasePlugin):
                     result.local_plugin_data[plugin_name()]
                 )
                 plugin.keywords.append(keywords[i // N])
-        return results
-
-    def process_concurrent(
-            self, results: list[Result], global_plugin_data: GlobalPluginData):
-        results_to_process = [
-            r for r in results if self.requires_processing(r)
-        ]
-        if len(results_to_process) == 0:
-            return results
-        N = len(results_to_process)
-        logger.info(f"Processing {N} results with concurrent")
-        prompts: list[str] = []
-        for keyword, topic in self.topics.items():
-            logger.info(f"Creating prompts related to {topic}...")
-            prompts.extend([
-                self.create_prompt(topic, r) for r in results_to_process
-            ])
-        logger.info("Sending prompts to the agent...")
-        responses = self.agent.complete_concurrent(prompts)
-        logger.info("Processing responses...")
-        keywords = list(self.topics.keys())
-        for i, r in enumerate(responses):
-            result = results_to_process[i % N]
-            keyword = keywords[i // N]
-            topic = self.topics[keyword]
-            msg = f"{result.title} is related to {topic}"
-            if "**RESULT: TRUE**" in r or r == "":
-                plugin: LanguageModelBasedKeywordsFilterData = (
-                    result.local_plugin_data[plugin_name()]
-                )
-                plugin.keywords.append(keyword)
-                logger.info(f"TRUE: {msg}")
-            else:
-                logger.info(f"FALSE: {msg}")
         return results
 
     def process_single(
@@ -168,10 +141,11 @@ class LanguageModelBasedKeywordsFilter(BasePlugin):
             return results
         N = len(results_to_process)
         logger.info(f"Processing {N} results in single...")
-        for keyword, topic in self.topics.items():
-            logger.info(f"Processing {topic}...")
+        for keyword, interested in self.interested_topics.items():
+            logger.info(f"Processing {interested}...")
+            discarded = self.discarded_topics.get(keyword, "")
             for i, result in enumerate(results_to_process):
-                prompt = self.create_prompt(topic, result)
+                prompt = prepare_prompts([result], interested, discarded)[0]
                 logger.info(
                     f"Processing {i+1}-th of {N} paper of keyword {keyword}..."
                 )
@@ -186,12 +160,38 @@ class LanguageModelBasedKeywordsFilter(BasePlugin):
                     logger.info(f"FALSE: Keyword {keyword} in {result.title}")
         return results
 
-    def create_prompt(self, topic: str, result: Result):
+    def requires_processing(self, result: Result):
+        plugin_datas: dict[str, BasePluginData] = result.local_plugin_data
+        for data in plugin_datas.values():
+            if isinstance(data, BaseKeywordsFilterData):
+                if data.plugin_name == plugin_name():
+                    continue
+                interested = set(self.interested_topics.keys())
+                ignorance = set(data.ignorance)
+                detected = set(data.keywords)
+                if len(interested & ignorance) > 0:
+                    # If the paper should be ignored even if it is classified
+                    # as interested, then it should be ignored.
+                    return False
+                if len(interested & detected) > 0:
+                    # If the paper has been classified as interested and
+                    # it is not ignored, then it should be processed later.
+                    return True
+                # If there exists a keyword filter but the paper is not
+                # classified as interested, then it should be ignored.
+                return False
+        # If there is no keyword filter before, then the paper should be
+        # processed.
+        return True
+
+
+def prepare_prompts(
+        results: list[Result], interested_topic: str, discarded_topic: str):
+    total_prompts: list[str] = []
+    for result in results:
         prompt = default_prompt_template()
         prompt = prompt.format(
-            topic=topic, title=result.title, abstract=result.summary
-        )
-        return prompt
-
-    def requires_processing(self, result: Result):
-        return True
+            interested=interested_topic, discarded=discarded_topic,
+            title=result.title, abstract=result.summary)
+        total_prompts.append(prompt)
+    return total_prompts
